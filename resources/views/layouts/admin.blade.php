392a1544b5186fe6f1cdfbdfc984328ca6d239d2
Sebastian Andres Cornejo Zamora <sebastian.cornejo@walmart.com>
2021-07-04 23:57:05 -0400

    try fix js

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -37,7 +37,7 @@
     <link rel="stylesheet" href="{{asset('css/principal.css')}}">
 
     <!-- Jcrop -->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
     <link rel="stylesheet" type="text/css" href="{{asset('css/cropit/cropit.css')}}">
     <!-- end Jcrop -->
 
