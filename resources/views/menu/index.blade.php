1823ac238632c2c2b2989ebbb4b3e6b828e8be04
root <sebastian.cornejo1@walmart.com>
2024-06-25 16:23:17 +0000

    add cortina

--- resources/views/menu/index.blade.php
+++ resources/views/menu/index.blade.php
@@ -1,71 +1,19 @@
-@extends ('layouts.admin')
-@section ('contenido')
-
 <style>
 
-	/* sacar a un nuevo componente */
-	.spacing{
-		width: 100%;
-		height: 120px;
-	}
+body, html {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+        }
 
-	.spacing {
-    	position: relative;
-	}
-
-	.spacing:before {
-		content: "";
-		background: #ab2f0f;
-		bottom: 0;
-		height: 2px;
-		left: 30%;
-		width: 40%;
-		top: 60%;
-		position: absolute;
-		transition: all 0.6s;
-	}
+	.full-width-img {
+            display: block;
+            width: 100vw;
+            height: auto;
+        }
 </style>
 
-	<link rel="stylesheet" href="{{asset('css/menu/menu.css?v=1.0')}}">
 
-	<div class="row">
-		<div class="col-lg-6 col-sm-8 col-md-6 col-xs-12 center-block">
-			<div class="panel-heading titulo_login">Elige tus favoritos</div>
-			{{-- @include('menu.search') --}}
-		</div>
-	</div>
 
-	<div class="row">
-		<div>
-			
-		</div>
-		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-			<center>
-				@foreach($categories as $category)
-					@if($category->products->count() > 0)
-						<h1 class="category">{{$category->nombre}}</h1>
-						<div class="category-descripcion">{{$category->descripcion}}</div>
-						<div class="">
-								@foreach($category->products as $product)
-								<div class="item-menu">
-									<div class="caja-item">
-										<div>
-											<div class="nombre">{{ $product->nombre}}</div>
-											<div class="descripcion">{{ $product->descripcion}}</div>
-										</div>
-										<div class="precio">${{$product->precio}}</div>		
-									</div>
-								</div>
-								@include('menu.modal-ordenar' , ['complements' => $product->complements] )
-								@endforeach
-						</div>
-						<div class="category-descripcion-secundaria">{{$category->descripcion_secundaria}}</div>
-					@endif
-				@endforeach
-			</center>
-		</div>
-	</div>
-	<div class="spacing" >
-
-	</div>
-@endsection
+<img src="{{ asset('images/temp/cortina_oasis_1.png') }}" alt="Cortina para nueva apertura" class="full-width-img">
---
+++ resources/views/menu/index_bk.blade.php
@@ -0,0 +1,71 @@
+@extends ('layouts.admin')
+@section ('contenido')
+
+<style>
+
+	/* sacar a un nuevo componente */
+	.spacing{
+		width: 100%;
+		height: 120px;
+	}
+
+	.spacing {
+    	position: relative;
+	}
+
+	.spacing:before {
+		content: "";
+		background: #ab2f0f;
+		bottom: 0;
+		height: 2px;
+		left: 30%;
+		width: 40%;
+		top: 60%;
+		position: absolute;
+		transition: all 0.6s;
+	}
+</style>
+
+	<link rel="stylesheet" href="{{asset('css/menu/menu.css?v=1.0')}}">
+
+	<div class="row">
+		<div class="col-lg-6 col-sm-8 col-md-6 col-xs-12 center-block">
+			<div class="panel-heading titulo_login">Elige tus favoritos</div>
+			{{-- @include('menu.search') --}}
+		</div>
+	</div>
+
+	<div class="row">
+		<div>
+			
+		</div>
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<center>
+				@foreach($categories as $category)
+					@if($category->products->count() > 0)
+						<h1 class="category">{{$category->nombre}}</h1>
+						<div class="category-descripcion">{{$category->descripcion}}</div>
+						<div class="">
+								@foreach($category->products as $product)
+								<div class="item-menu">
+									<div class="caja-item">
+										<div>
+											<div class="nombre">{{ $product->nombre}}</div>
+											<div class="descripcion">{{ $product->descripcion}}</div>
+										</div>
+										<div class="precio">${{$product->precio}}</div>		
+									</div>
+								</div>
+								@include('menu.modal-ordenar' , ['complements' => $product->complements] )
+								@endforeach
+						</div>
+						<div class="category-descripcion-secundaria">{{$category->descripcion_secundaria}}</div>
+					@endif
+				@endforeach
+			</center>
+		</div>
+	</div>
+	<div class="spacing" >
+
+	</div>
+@endsection
